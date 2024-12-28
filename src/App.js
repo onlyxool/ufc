6786a667fd35334b5d28a444d281e49d5c459074
Kyle <onlyxool@gmail.com>
2024-12-28 15:58:00 -0500

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
+import Description from './Description';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="intro">
+        <h1>UFC MATCH PREDICTION</h1>
+        <Description /> {/* 使用新组件 */}
+        <ul className="actions">
+          <li><a href="#header" className="button icon solid solo fa-arrow-down scrolly">Continue</a></li>
+        </ul>
     </div>
   );
 }
